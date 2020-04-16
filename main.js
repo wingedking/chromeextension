daaa46a1dca105d46738afe11b851b0a6b847f4d
Tommy Han <tommy@Tommys-Air.home>
2020-04-16 01:16:56 -0400

    fixed css colors.

--- main.js
+++ main.js
@@ -60,7 +60,22 @@ function addCSSToPage(){
     body {
         background: #121212;
         font-size: 24px;
-    }`
+        margin: 0px;
+    }
+    .footer {
+        width:100%;
+        bottom: 0px;
+        position: fixed;
+        display:flex;
+        justify-content: space-around;
+        align-content: flex-end;
+        background: #121212;
+    }
+
+    label {
+        color: yellow;
+    }
+    `
    const style = document.createElement('style');
    style.type = 'text/css';
    style.innerHTML = code;
@@ -69,7 +84,7 @@ function addCSSToPage(){
 window.addEventListener('load', function(){
     const body = document.querySelector('body');
     const classList = body.classList;
-    if(classList[0] === 'vsc-initialized'){
+    // if(classList[0] === 'vsc-initialized'){
         const firstChild = body.firstChild;
         if(firstChild.tagName === 'PRE'){
             const innerText = firstChild.innerText;
@@ -78,6 +93,68 @@ window.addEventListener('load', function(){
             addCSSToPage();
             console.log('huh??');
             firstChild.innerHTML = htmlJSON(obj, 0);
+            changeColor(body);
         }
-    }
+    // }
+    
 })
+
+{/* <div class=footer>
+<label> Background:<input class="backgroundColor" type="color" name="color1" value="#121212"></label>
+<label> Curly Brackets:<input class="bracketsColor" type="color" name="color2" value="#03DAC6"></label>
+<label> Keys:<input class="keysColor" type="color" name="color3" value="#FFFF00"</label>
+<label> Values:<input class="valuesColor" type="color" name="color4" value="#009df9"></label>
+<label> Colon: <input class="colonColor" type="color" name="color5" value="#CF6679"></label>
+</div> */}
+
+function changeColor(body){
+
+    body.innerHTML += `<div class="footer">
+    <label> Background:<input class="backgroundColor" type="color" name="color1" value="#121212"></label>
+    <label> Curly Brackets:<input class="bracketsColor" type="color" name="color2" value="#03DAC6"></label>
+    <label> Keys:<input class="keysColor" type="color" name="color3" value="#FFFF00"></label>
+    <label> Values:<input class="valuesColor" type="color" name="color4" value="#009df9"></label>
+    <label> Colon: <input class="colonColor" type="color" name="color5" value="#CF6679"></label>
+    </div>`
+    
+    
+    backgroundColor = document.querySelector(".backgroundColor");
+    footerColor = document.querySelector(".footer");
+    console.log("************ here in changeColor *******************")
+    bracketsColor = document.querySelector(".bracketsColor");
+    keysColor = document.querySelector(".keysColor");
+    valuesColor = document.querySelector(".valuesColor");
+    colonColor = document.querySelector(".colonColor");
+
+    backgroundColor.addEventListener("input", function(){
+        console.log("background color entered")
+        const body = document.querySelector("body");
+        console.log("background color", backgroundColor.value);
+        body.style.backgroundColor = backgroundColor.value;
+        footerColor.style.backgroundColor = backgroundColor.value;
+    });
+    
+    bracketsColor.addEventListener("input", function(){
+        console.log("bracketsColor entered");
+        document.styleSheets[0].cssRules[0].style.color = bracketsColor.value; // for curly brace
+        document.styleSheets[0].cssRules[4].style.color = bracketsColor.value;
+    });
+    
+    keysColor.addEventListener("input", function(){
+        console.log(keysColor.value)
+        document.styleSheets[0].cssRules[1].style.color = keysColor.value; // for key name
+    });
+    
+    valuesColor.addEventListener("input", function(){
+        console.log(valuesColor.value)
+        document.styleSheets[0].cssRules[2].style.color = valuesColor.value; // value
+    });
+    
+    colonColor.addEventListener("input", function(){
+        console.log(colonColor.value)
+        document.styleSheets[0].cssRules[3].style.color = colonColor.value;
+    });
+}
+
+
+
